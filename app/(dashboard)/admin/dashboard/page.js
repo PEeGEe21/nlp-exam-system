d990a14d1f83af4c7eb440a7887b931d66e1618c
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2024-09-11 00:11:22 +0100

    added appwrite configuration

--- app/(dashboard)/admin/dashboard/page.js
+++ app/(dashboard)/admin/dashboard/page.js
@@ -1,6 +1,18 @@
-import React from 'react'
+"use client"
+import React, {useEffect} from 'react'
+import { db } from '@/app/appwrite/database'
 
 const Dashboard = () => {
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  const init = async () => {
+    const response = await db.notes.list()
+    console.log(response.documents)
+  }
+
   return (
     <div>
       Dashboard
