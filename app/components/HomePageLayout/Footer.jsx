b6dc1ab739e315623634ebe91e712172c73389bf
praise <mailpraiseudeh@gmail.com>
2024-09-25 14:39:44 +0100

    updated landing page

--- app/components/HomePageLayout/Footer.jsx
+++ app/components/HomePageLayout/Footer.jsx
@@ -1,12 +1,7 @@
 import Link from "next/link";
-import { ImageBase, Paragraph, ViewMore } from "../Button/Button";
-import {
-    TwitterIcon,
-    YoutubeIcon,
-    LinkedInIcon,
-    TelegramIcon,
-} from "../IconComponent";
+
 import Image from "next/image";
+import { TwitterIcon, YoutubeIcon } from "../ui/IconComponent";
 
 const Footer = () => {
     return (
@@ -14,68 +9,19 @@ const Footer = () => {
             <div className="container mx-auto px-4">
                 <div className="flex flex-col ">
                     <div className="flex-1">
-                        <div className="flex items-center  justify-between mb-6 flex-wrap">
-                            <div className="w-full md:w-1/4 flex items-center mb-4 md:mb-0 ">
-                                <Image
+                        <div className="flex items-center  justify-center mb-6 flex-wrap">
+                            <div className="w-full flex items-center mb-4 md:mb-0 justify-center ">
+                                {/* <Image
                                     src="/images/Logofooter.png"
                                     alt="decoration barrel"
                                     width={200}
                                     height={150}
-                                />
-                            </div>
-
-                            <div className="flex justify-between w-full md:w-3/4 flex-wrap gap-3">
-                                <ul className="flex w-full lg:text-base text-sm md:w-3/4 space-x-5 flex-wrap mt-0 justify-start md:justify-end items-center">
-                                    <li>
-                                        <a href="mailto:info@mailpraiseudeh@gmail.com">
-                                            Contact
-                                        </a>
-                                    </li>
-                                </ul>
-                                <div className="flex space-x-6">
-                                    {/* <Link
-                                        href="https://t.me/+ZF5ep5gBwyYxZDdk"
-                                        target="_blank"
-                                        className="cursor-pointer"
-                                    >
-                                        <TelegramIcon className={"w-6 h-6"} />
-                                    </Link> */}
-                                    {/* <InstagramIcon className={"w-6 h-6"} /> */}
-
-                                    {/* <LinkedInIcon className={"w-6 h-6"} /> */}
-
-                                    <YoutubeIcon className={"w-6 h-6"} />
-
-                                    <TwitterIcon className={"w-6 h-6"} />
-                                </div>
+                                /> */}
+                                Exam System
                             </div>
                         </div>
-                        {/* <div className="">
-             
-              <p>Paragraph 1</p>
-              <p>Paragraph 2</p>
-              <p>Paragraph 3</p>
-              <p>Paragraph 4</p>
-            </div> */}
                     </div>
-                    <div
-                        className="text-xs"
-                        style={{
-                            borderTop: "1px solid #737272",
-                            paddingTop: "1.5rem",
-                            lineHeight: "2",
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "2rem",
-                        }}
-                    >
-                        <Paragraph
-                            text={
-                                "lorem ipsum"
-                            }
-                        />
-                    </div>
-                    <div className="relative flex pt-[6rem] justify-between flex-wrap">
+                    <div className="relative flex pt-2 justify-center flex-wrap text-center">
                         <p
                             style={{
                                 color: "#878787",
@@ -83,38 +29,9 @@ const Footer = () => {
                                 lineHeight: "2",
                             }}
                         >
-                            &copy; {new Date().getFullYear()} Peegee. All
+                            &copy;{new Date().getFullYear()} P-R. All
                             rights reserved
                         </p>
-                        <div className="flex space-x-2 flex-wrap">
-                            {/* <ViewMore
-                                address={""}
-                                text={"Disclosures"}
-                                style={{
-                                    color: "#878787",
-                                    fontSize: "1rem",
-                                    lineHeight: "2",
-                                }}
-                            /> */}
-                            <ViewMore
-                                address={"/privacy-policy"}
-                                text={"Cookie Policy"}
-                                style={{
-                                    color: "#878787",
-                                    fontSize: "1rem",
-                                    lineHeight: "2",
-                                }}
-                            />
-                            <ViewMore
-                                address={"/privacy-policy"}
-                                text={"Privacy Policy"}
-                                style={{
-                                    color: "#878787",
-                                    fontSize: "1rem",
-                                    lineHeight: "2",
-                                }}
-                            />
-                        </div>
                     </div>
                 </div>
             </div>
--- app/components/HomePageLayout/MainComponent.jsx
+++ app/components/HomePageLayout/MainComponent.jsx
@@ -3,25 +3,30 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const MainComponent = () => {
   return (
     <>
         <Navbar/>
-        <section className="px-4 text-center mt-4 sm:mt-10 md:mt-14 xl:mt-20 max-w-6xl mx-auto">
+        <section className="px-4 text-center mt-4 sm:mt-10 md:mt-14 xl:mt-20 max-w-6xl mx-auto pb-20">
             <h1 className="text-3xl font-bold text-gray-900 md:text-4xl xl:text-6xl xl:leading-tight"> 
                 We prepare the best testing experience for you...<br className="hidden sm:inline"/> 
             </h1>
-            <h2 className="mt-6 leading-snug text-gray-500 xl:mt-5 xl:text-xl"> 
-                Write an an exam 
-            </h2>
             <div className="relative mt-6 flex flex-col items-center justify-center gap-2">
                 <div className="flex items-center justify-between text-base">
                     <Link className="bg-[#373636] border border-[#373636] text-[#fff] px-6 py-2 text-base rounded-lg" href="/auth/signup">Get Started</Link>
                     
                 </div>
             </div>
+            <div className="relative mt-6 flex flex-col items-center justify-center gap-2 rounded-lg">
+                <div className='h-[600px] w-full rounded-lg'>
+                    <Image src={'/images/create-test.png'} alt='create-test'  fill className='object-contain object-top w-full h-full rounded-lg' />
+                </div>
+            </div>
+
         </section>
+        <Footer/>
     </>
   )
 }
